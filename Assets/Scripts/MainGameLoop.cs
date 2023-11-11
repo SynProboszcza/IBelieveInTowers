322cd9a778a640c2f8f779e569fae8a5af7c0103
SynProboszcza <miczuja@gmail.com>
2023-11-11 15:35:58 +0100

    NodeMesh improvements
    Generating mesh of ShopNodes now excludes obstacles (still generates on enemy path)

--- Assets/Scripts/MainGameLoop.cs
+++ Assets/Scripts/MainGameLoop.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using Unity.VisualScripting;
 using UnityEngine;
 
 public class MainGameLoop : MonoBehaviour
 {
     public GameObject nodePrefab;
+    public List<GameObject> nodes = new List<GameObject>();
 
     // playArenaCorners is an object that has 4 properly named child gameObjects
     // their transform.position 's are corners of play arena
@@ -16,6 +18,9 @@ public class MainGameLoop : MonoBehaviour
     public Vector2 topRight;
     public Vector2 bottomLeft;
     public Vector2 bottomRight;
+    public int nodesInstantiated = 0;
+    public int nodesDestroyed = 0;
+    public bool isDebugging = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -39,7 +44,8 @@ public class MainGameLoop : MonoBehaviour
 
     private void GenerateAndConfigureNodeMesh(Vector2 topleft, Vector2 bottomright)
     {
-        if (nodePrefab != null) { 
+        nodesInstantiated = 0;
+        if (nodePrefab != null) {
             Vector2 distance = (new Vector2(Mathf.Abs(topleft.x), Mathf.Abs(topleft.y))) - new Vector2(Mathf.Abs(bottomright.x), Mathf.Abs(bottomright.y));
             distance = new Vector2(Mathf.Abs(distance.x), Mathf.Abs(distance.y));
             for (int x = 0; x <= distance.x; x++)
@@ -48,16 +54,26 @@ public class MainGameLoop : MonoBehaviour
                 {
                     //it would be better to check if node is on
                     //forbidden tile, then skip that tile
-                    //if ()
-                    //{
-                    //
-                    //}
-                    GameObject node = Instantiate(nodePrefab, new Vector3(topleft.x + x, bottomright.y + y, -1f), Quaternion.identity);
+                    GameObject singleNode = Instantiate(nodePrefab, new Vector3(topleft.x + x, bottomright.y + y, -1f), Quaternion.identity);
                     //configure node right here and right now
                     //reduce number of nodes - we cant place them on:
                     //paths, obstacles, spawners, despawners,
                     //other turrets need to be configured to not let any more
                     //or they need to be upgradable
+                    nodesInstantiated++;
+                    nodes.Add(singleNode);
+                }
+            }
+            foreach (GameObject node in nodes)
+            {
+                foreach (Transform obstacle in obstacles)
+                {
+                    //if(node.transform.position == obstacle.transform.position)
+                    if(Mathf.Approximately(node.transform.position.x, obstacle.transform.position.x) && Mathf.Approximately(node.transform.position.y, obstacle.transform.position.y))
+                    {
+                        Destroy(node.gameObject);
+                        nodesDestroyed++;
+                    }
                 }
             }
         } 
@@ -65,7 +81,7 @@ public class MainGameLoop : MonoBehaviour
         {
             Debug.Log("nie mamy nodes");
         }
-        Debug.Log("function ended");
+        Debug.Log("function ended, instantiated: " + nodesInstantiated);
     }
 
     private void GetPlayArenaCorners()
@@ -78,18 +94,22 @@ public class MainGameLoop : MonoBehaviour
         {
             if (child.name == "topLeft")
             {
+                if (isDebugging) { Debug.Log("got topleft"); }
                 topLeft = child.transform.position;
             }
             else if (child.name == "topRight")
             {
+                if (isDebugging) { Debug.Log("got topright"); }
                 topRight = child.transform.position;
             }
             else if (child.name == "bottomLeft")
             {
+                if (isDebugging) { Debug.Log("got botleft"); }
                 bottomLeft = child.transform.position;
             }
             else if (child.name == "bottomRight")
             {
+                if (isDebugging) { Debug.Log("got botright"); }
                 bottomRight = child.transform.position;
             }
         }
