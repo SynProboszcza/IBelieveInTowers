10244d0ab8b92cf06549b2d5259e975b25bf512f
SynProboszcza <miczuja@gmail.com>
2023-12-04 12:19:17 +0100

    Setting up nicknames and connection to Master server, discarding test connection and making it automatic

--- Assets/Scripts/UI/Rooms/CreateRoomMenu.cs
+++ Assets/Scripts/UI/Rooms/CreateRoomMenu.cs
@@ -11,36 +11,69 @@ public class CreateRoomMenu : MonoBehaviourPunCallbacks
 
     [SerializeField]
     private TMP_InputField _roomName;
+    [SerializeField]
+    private TMP_InputField _nickName;
     public GameObject showConnection;
+    public string backupNickNamePrefix = "defaultNickname";
+    public string gameVersion = "0.1";
 
+
+    void Start()
+    {
+        print("Connecting to server...");
+        PhotonNetwork.GameVersion = gameVersion;
+        PhotonNetwork.ConnectUsingSettings();
+    }
 
     public void OnClickCreateRoom()
     {
         if (!PhotonNetwork.IsConnectedAndReady)
         {
             Debug.Log("Not Connected, aborting creating room");
-
-            showConnection.GetComponent<TMP_Text>().text = "Connection is null or not ready yet!";
+            showConnection.GetComponent<TMP_Text>().text = "Can't connect or not ready yet!";
             return;
         }
         RoomOptions options = new RoomOptions();
         options.MaxPlayers = 2;
-        print(":::" + _roomName.text + ":::");
+        
+        // Checking for room name, if not exists set up default
         if (_roomName.text == "")
         {
             _roomName.text = "DefaultRoomName";
         }
-        print(":::" + _roomName.text + ":::");
+
+        // Checking for nickname, if not exists set up default
+        // There is minimum length for nickname specified by Dawid,
+        // not demanded by Photon
+        if(_nickName.text.ToString().Length > 3)
+        {
+            PhotonNetwork.NickName = _nickName.text.ToString();
+        } else
+        {
+            backupNickNamePrefix += Random.Range(0, 55555).ToString();
+            PhotonNetwork.NickName = backupNickNamePrefix;
+        }
         PhotonNetwork.CreateRoom(_roomName.text, options, TypedLobby.Default);
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        showConnection.GetComponent<TMP_Text>().text = "Connected to master";
+        base.OnConnectedToMaster();
     }
 
     public override void OnCreatedRoom()
     {
-        // TODO: change scene to appropriate room
-
-        showConnection.GetComponent<TMP_Text>().text = "Room created with name: "+ _roomName.text +"\n Joining...";
+        showConnection.GetComponent<TMP_Text>().text = "Joining room: " + _roomName.text + " as " + PhotonNetwork.LocalPlayer.NickName + "...";
         PhotonNetwork.JoinLobby();
         base.OnCreatedRoom();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        // TODO change scene here
+        print("Joined Lobby" + PhotonNetwork.CurrentLobby.ToString());
+        base.OnJoinedLobby();
     }
 
     public override void OnCreateRoomFailed(short returnCode, string message)
@@ -49,10 +82,10 @@ public class CreateRoomMenu : MonoBehaviourPunCallbacks
         base.OnCreateRoomFailed(returnCode, message);
     }
 
-    public override void OnConnectedToMaster()
+    public override void OnDisconnected(DisconnectCause cause)
     {
-        showConnection.GetComponent<TMP_Text>().text = "Connected to master!";
-
-        base.OnConnectedToMaster();
+        print("Disconnected because: " + cause.ToString());
+        base.OnDisconnected(cause);
     }
+
 }
