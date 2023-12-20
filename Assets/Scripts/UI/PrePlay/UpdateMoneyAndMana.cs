17b29c7b5243dd4aa2aa2b0b0dc8cf492e29b2e2
SynProboszcza <miczuja@gmail.com>
2023-12-20 16:43:26 +0100

    Mana and Money are updated every frame

---
+++ Assets/Scripts/UI/PrePlay/UpdateMoneyAndMana.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class UpdateMoneyAndMana : MonoBehaviour
+{
+    [SerializeField]
+    private TMP_Text moneyText;
+    [SerializeField]
+    private TMP_Text manaText;
+
+    void Update()
+    {
+        UpdateManaAndMoney();
+    }
+    public void UpdateManaAndMoney()
+    {
+        moneyText.text = "GOLD: " + CrossSceneManager.instance.playerMoney;
+        manaText.text = "MANA: " + CrossSceneManager.instance.playerMana;
+    }
+}
