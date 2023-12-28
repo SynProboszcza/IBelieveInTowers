23bfa6208886f607c5f600fb51cbf2a5199116e0
SynProboszcza <42191340+SynProboszcza@users.noreply.github.com>
2023-12-28 21:30:13 +0100

    Update PreMainGame.cs

--- Assets/Scripts/PreMainGame.cs
+++ Assets/Scripts/PreMainGame.cs
@@ -276,7 +276,7 @@ public class PreMainGame : MonoBehaviourPunCallbacks, IPunObservable
             print("ENEMY load progress: " + _enemyLoadProgress);
             //if (amIMaster)
             //{
-                if (_enemyLoadProgress >= 0.8f && this.mapLoadProgress >= 0.8f) // floating point math smh
+                if (this.mapLoadProgress >= 0.8f && _enemyLoadProgress >= 0.8f) // floating point math smh
                 {
                     gameObject.GetComponent<PhotonView>().RPC("AllowToChangeScene", RpcTarget.All);
                 }
