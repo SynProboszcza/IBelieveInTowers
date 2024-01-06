64e8e0cffb590a95626616f889992fcac32b01b6
SynProboszcza <miczuja@gmail.com>
2024-01-06 16:02:36 +0100

    Removed compile error; Added balancing csv to reference known good values

--- Assets/Scripts/CrossSceneManager.cs
+++ Assets/Scripts/CrossSceneManager.cs
@@ -3,7 +3,6 @@ using Photon.Realtime;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
-using UnityEditor.VersionControl;
 using UnityEngine;
 //using UnityEngine.SocialPlatforms.GameCenter;
 
